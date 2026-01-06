8153408dcd9afd001c51c208cc31eec30c42c9ee
man92C <am.mann@hotmail.com>
2026-01-06 05:10:18 -0500

    task finished user management with react frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
-import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+import Register from "./components/Register/Register";
+import LoginPanel from "./components/Login/Login"
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
 }
+
 export default App;
+
