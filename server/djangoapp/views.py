d5d862453003a6e50553a8f8775d268b73a86354
A-M M. <112853957+man92C@users.noreply.github.com>
2026-01-09 07:30:36 +0100

    Fix formatting in get_dealerships function

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -84,8 +84,8 @@ def registration(request):
 # Update the `get_dealerships` view to render
 # the index page with a list of dealerships
 def get_dealerships(
-    request, 
-    state="All"
+    request,
+    state="All",
 ):
     if state == "All":
         endpoint = "/fetchDealers"
